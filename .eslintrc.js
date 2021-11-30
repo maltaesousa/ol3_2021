7134d764122a335de37dec055fc6749448d9ed9c
maltaesousa <stephane.maltaesousa@ne.ch>
2021-11-30 09:29:34 +0100

    init lint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: false,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  plugins: [
+    'html'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
+};
